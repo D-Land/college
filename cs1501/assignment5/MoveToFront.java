507db304f1a7276f56427a27d61551c033485109
Drew Land <drewmland@gmail.com>
2013-11-28 21:06:35 -0500

    Fix bugs in MTF

--- cs1501/assignment5/MoveToFront.java
+++ cs1501/assignment5/MoveToFront.java
@@ -16,43 +16,44 @@ public class MoveToFront{
       System.out.println("Error: Expected + or - as argument.");
       System.exit(1);
     }
+  }
 
-    public static void encode(){
-      ArrayList<Character> alphabet = alphabet();
-      Character input;
-      int index;
+  public static void encode(){
+    ArrayList<Character> alphabet = alphabet();
+    Character input;
+    int index;
 
-      while(!BinaryStdIn.isEmpty()){
-        input = (Character)BinaryStdIn.readChar();
-        index = (int)alphabet.indexOf(input);
-        BinaryStdOut.write(index);
+    while(!BinaryStdIn.isEmpty()){
+      input = (Character)BinaryStdIn.readChar();
+      index = (int)alphabet.indexOf(input);
+      BinaryStdOut.write(index);
 
-        alphabet.remove(index);
-        alphabet.add(0, input);
-      }
-    }
-
-    public static void decode(){
-      ArrayList<Character> alphabet = alphabet();
-      int index;
-      char output;
-
-      while(!BinaryStdIn.isEmpty()){
-        index = BinaryStdIn.readChar();
-        output = (char)alphabet.get(index);
-        BinaryStdOut.write(output);
-
-        alphabet.remove(index);
-        alphabet.add(0, output);
-      }
-    }
-
-    private static void alphabet(){
-      ArrayList<Character> alphabet = new ArrayList<Character>(256);
-
-      for(char i = 0; i < 256; i++){
-        alphabet.add((Character)i);
-      }
-      return alphabet;
+      alphabet.remove(index);
+      alphabet.add(0, input);
     }
   }
+
+  public static void decode(){
+    ArrayList<Character> alphabet = alphabet();
+    int index;
+    char output;
+
+    while(!BinaryStdIn.isEmpty()){
+      index = BinaryStdIn.readChar();
+      output = (char)alphabet.get(index);
+      BinaryStdOut.write(output);
+
+      alphabet.remove(index);
+      alphabet.add(0, output);
+    }
+  }
+
+  private static ArrayList<Character> alphabet(){
+    ArrayList<Character> alphabet = new ArrayList<Character>(256);
+
+    for(char i = 0; i < 256; i++){
+      alphabet.add((Character)i);
+    }
+    return alphabet;
+  }
+}
